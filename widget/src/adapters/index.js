9118771cde548dba7473e463591cefcefc2143e8
welborn <djidjonouaflou@gmail.com>
2026-04-06 07:51:19 +0000

    Code betaia synchronisé sur development

--- widget/src/adapters/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Index des exports du module adapters
- * @module adapters
- */
-
-export { detectPreferredColorScheme, detectSiteTheme, extractCSSVariables, resolveTheme } from './themeDetector.js';
-export { lightPalette, darkPalette, getPalette, generateCSSVariables, mergePalette } from './colorPalettes.js';
-export { measureContainer, calculateOptimalAdCount, calculateOptimalLayout, createResizeObserver } from './spaceDetector.js';
-export { detectDeviceType, getBrowserInfo, getOSInfo, getScreenInfo, hasTouchSupport } from './deviceDetector.js';
--- widget/src/api/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Index des exports du module api
- * @module api
- */
-
-export { createAdsClient } from './adsClient.js';
-export { createEndpoints, detectBaseUrl, isValidEndpoint } from './endpoints.js';
-export { createAdsCache } from './adsCache.js';
-export { createPublicApi, exposePublicApi, isWidgetLoaded } from './publicApi.js';
